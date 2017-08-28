3d691cb33aa04c5507cc634866dadac547e53856
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-08-28 17:07:55 -0600

    added db design scaffolding

---
+++ app/Http/Controllers/MentorTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\mentor_type;
+use Illuminate\Http\Request;
+
+class MentorTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\mentor_type  $mentor_type
+     * @return \Illuminate\Http\Response
+     */
+    public function show(mentor_type $mentor_type)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\mentor_type  $mentor_type
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(mentor_type $mentor_type)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\mentor_type  $mentor_type
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, mentor_type $mentor_type)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\mentor_type  $mentor_type
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(mentor_type $mentor_type)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RequirementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\requirement;
+use Illuminate\Http\Request;
+
+class RequirementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\requirement  $requirement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(requirement $requirement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\requirement  $requirement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(requirement $requirement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\requirement  $requirement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, requirement $requirement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\requirement  $requirement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(requirement $requirement)
+    {
+        //
+    }
+}
