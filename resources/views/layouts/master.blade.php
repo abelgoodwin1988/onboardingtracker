486f98059b6291895aae725012d8c2d4d68cbcba
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-08-29 18:23:53 -0600

    pulled in bulma, scaffolding for master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Onbarding Tracker</title>
+  </head>
+  <body>
+    {{-- left-aligned thin navbar --}}
+    {{-- header sections for additional details --}}
+    {{-- content section --}}
+      {{-- forms --}}
+
+
+  </body>
+</html>
