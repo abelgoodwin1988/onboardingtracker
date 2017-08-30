30ff40802070c861424f0d5731e2c3de1b649a77
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-08-30 09:51:12 -0600

    updated navbar to font-awesome icons. So preeny!

--- resources/views/layouts/master_navbar.blade.php
+++ resources/views/layouts/master_navbar.blade.php
@@ -1,20 +1,30 @@
 <div class="column is-1 is-gapless has-text-centered" id="master_navbar">
 
-  <ul>
+  <ul class=>
     <li>
-      <span class="icon is-large"><i class="fa fa-home"></i></span>
+      <a>
+        <span class="icon is-large"><i class="fa fa-home"></i></span>
+      </a>
     </li>
     <li>
-      <a>navbar brand</a>
+      <a>
+        <span class="icon is-large"><i class="fa fa-user-plus"></i></span>
+      </a>
     </li>
     <li>
-      <a>Candidate</a>
+      <a>
+        <span class="icon is-large"><i class="fa fa-user"></i></span>
+      </a>
     </li>
     <li>
-      <a>Person</a>
+      <a>
+        <span class="icon is-large"><i class="fa fa-user-circle-o"></i></span>
+      </a>
     </li>
     <li>
-      <a>Look_ups</a>
+      <a>
+        <span class="icon is-large"><i class="fa fa-random"></i></span>
+      </a>
     </li>
   </ul>
 </div>
