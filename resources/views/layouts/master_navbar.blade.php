2088908b824aca22852f129537cd5e710bcabbb1
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-08-29 21:50:24 -0600

    structure scaffolding.

--- resources/views/layouts/master_navbar.blade.php
+++ resources/views/layouts/master_navbar.blade.php
@@ -1,13 +1,16 @@
 <div class="column is-1 is-gapless">
   <ul>
     <li>
+      <a>navBrand</a>
+    </li>
+    <li>
       <a>Candidate</a>
     </li>
     <li>
       <a>Person</a>
     </li>
     <li>
-      <a>Look ups</a>
+      <a>Look_ups</a>
     </li>
   </ul>
 </div>
