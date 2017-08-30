c574a88a5eb2989d6724ece92fb9e499ed52af48
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-08-29 21:45:19 -0600

    master page scaffolding.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,13 +3,36 @@
   <head>
     <meta charset="utf-8">
     <title>Onbarding Tracker</title>
+
+    {{-- CSS: --}}
+    {{-- CSS:Bulma --}}
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
   </head>
   <body>
+    <div class="columns is-mobile">
+
     {{-- left-aligned thin navbar --}}
+    @include('layouts.master_navbar')
     {{-- header sections for additional details --}}
     {{-- content section --}}
       {{-- forms --}}
 
+      <div class="column is-11 is-gapless">
+        <div class="columns">
+          <div class="column">
+            SubNav
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            Content
+          </div>
+        </div>
+      </div>
+    </div>
 
+    {{-- JS: Mixed JS file --}}
+    <script src="{{ mix('/js/app.js') }}"></script>
   </body>
 </html>
---
+++ resources/views/layouts/master_navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="column is-1 is-gapless">
+  <ul>
+    <li>
+      <a>Candidate</a>
+    </li>
+    <li>
+      <a>Person</a>
+    </li>
+    <li>
+      <a>Look ups</a>
+    </li>
+  </ul>
+</div>
