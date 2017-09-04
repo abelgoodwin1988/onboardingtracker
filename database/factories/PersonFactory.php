3b3855fed00fa4568f7bb8bc7e3ba7b2db65c4ef
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-09-04 14:55:32 -0600

    seeder and model factory in place.

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -2,8 +2,13 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Person::class, function (Faker $faker) {
     return [
         //
+        'name_first' => $faker->firstName,
+        'name_middle' => $faker->firstName,
+        'name_last' => $faker->lastName,
+        'email' => $faker->email,
+        'phone_number' => $faker->phoneNumber
     ];
 });
