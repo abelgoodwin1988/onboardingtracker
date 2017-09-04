4d4b974849ad17431ed18aad02add6ba9d825bf3
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-09-04 09:06:39 -0600

    added spacing to person_card

--- resources/views/people/partials/person_card.blade.php
+++ resources/views/people/partials/person_card.blade.php
@@ -8,42 +8,44 @@
  --}}
 
 @foreach ($people as $person)
-  <div class="card">
+  <div class="section">
+    <div class="card">
 
-    <div class="card-content">
-      <div class="columns">
-        <div class="column">
+      <div class="card-content">
+        <div class="columns">
+          <div class="column">
 
-          <p class="title">
-            {{$person->name_last}}, {{$person->name_first}} {{$person->name_middle}}
-          </p>
-          <p class="subtitle">
-            {{$person->email}}
-          </p>
-          <p class="subtitle">
-            {{$person->phone_number}}
-          </p>
+            <p class="title">
+              {{$person->name_last}}, {{$person->name_first}} {{$person->name_middle}}
+            </p>
+            <p class="subtitle">
+              {{$person->email}}
+            </p>
+            <p class="subtitle">
+              {{$person->phone_number}}
+            </p>
+          </div>
+
+          <div class="column">
+            <p class="subtitle">
+              Created: {{$person->created_at->toFormattedDateString()}}
+            </p>
+            <p class="subtitle">
+              Last Updated: {{$person->updated_at->toFormattedDateString()}}
+            </p>
+          </div>
+
         </div>
-
-        <div class="column">
-          <p class="subtitle">
-            Created: {{$person->created_at->toFormattedDateString()}}
-          </p>
-          <p class="subtitle">
-            Last Updated: {{$person->updated_at->toFormattedDateString()}}
-          </p>
-        </div>
-
       </div>
+
+      <footer class="card-footer">
+        <p class="card-footer-item">
+          <span>
+            <a href="/person_id={{$person->person_id}}">View Person</a>
+          </span>
+        </p>
+      </footer>
+
     </div>
-
-    <footer class="card-footer">
-      <p class="card-footer-item">
-        <span>
-          <a href="/person_id={{$person->person_id}}">View Person</a>
-        </span>
-      </p>
-    </footer>
-
   </div>
 @endforeach
