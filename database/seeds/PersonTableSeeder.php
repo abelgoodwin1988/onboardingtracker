184cdebe282e3d19cd4bab288dfa80a014fad773
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-09-04 13:55:36 -0600

    scaffolding for sample seeding - person.

--- database/seeds/PersonTableSeeder.php
+++ database/seeds/PersonTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class PersonTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(PersonTableSeeder::class);
+        //
+        factory('App\Person', 100)->create();
     }
 }
