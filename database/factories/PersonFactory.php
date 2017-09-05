7cfbd0b10adbc04c28cb91299d6ff37b6289dd40
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-09-04 18:17:38 -0600

    added nullability for deleted_at

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -9,6 +9,9 @@ $factory->define(App\Person::class, function (Faker $faker) {
         'name_middle' => $faker->firstName,
         'name_last' => $faker->lastName,
         'email' => $faker->email,
-        'phone_number' => $faker->phoneNumber
+        'phone_number' => $faker->phoneNumber,
+        'created_at' => $faker->dateTimeBetween($startDate = '-10 years', $endDate = '-3 years'),
+        'updated_at' => $faker->dateTimeBetween($startDate = '-10 years', $endDate = '-3 years'),
+        'deleted_at' => $faker->randomElement($array = array ($faker->dateTimeBetween($startDate = '-3 years', $endDate = 'now'),null))
     ];
 });
