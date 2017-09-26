c6e1b60b5f7f092cb3f13b11954751458060adf4
Abel Goodwin <abelgoodwin1988@gmail.com>
2017-09-26 15:32:06 -0600

    reclone to mobile workstart

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Person::class, function (Faker $faker) {
         'name_first' => $faker->firstName,
         'name_middle' => $faker->firstName,
         'name_last' => $faker->lastName,
-        'email' => $faker->email,
+        'email' => $faker->email, 
         'phone_number' => $faker->phoneNumber,
         'created_at' => $faker->dateTimeBetween($startDate = '-10 years', $endDate = '-3 years'),
         'updated_at' => $faker->dateTimeBetween($startDate = '-10 years', $endDate = '-3 years'),
