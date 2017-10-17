e42ef211f73279adcee238f36e35439702b81abb
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-10-16 18:56:00 -0600

    upd ppl view

--- resources/views/people/show_people/index.blade.php
+++ resources/views/people/show_people/index.blade.php
@@ -19,21 +19,27 @@
   {{-- enable toggling between views --}}
   <div class="field has-addons">
     <p class="control">
-      <a class="button">
-        <span class="icon is-small">
-          <i class="fa fa-address-card-o"></i>
-        </span>
-      </a>
-    </p>
-
-    <p class="control">
-      <a class="button">
+      <a class="button is-active" id="btnList" name="btnList">
         <span class="icon is-small">
           <i class="fa fa-list"></i>
         </span>
       </a>
     </p>
+
+    <p class="control">
+      <a class="button" id="btnCard" name="btnCard">
+        <span class="icon is-small">
+          <i class="fa fa-address-card-o"></i>
+        </span>
+      </a>
+    </p>
   </div>
 
-  @include('people.partials.person_table')
+  @include('people.partials.person_card')
 @endsection
+<script>
+  $(".control > .button").click(function(){
+      $(".control > .button").removeClass("is-active");
+      $(this).addClass("is-active");
+  });
+</script>
